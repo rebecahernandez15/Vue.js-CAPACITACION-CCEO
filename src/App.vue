9706e43292b09092488d68175323722e39d6a02d
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-03 16:07:54 -0600

    feat (challenge.p2): ✨ End of the challenge

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,32 @@ const counter = ref(0);
 const increment = () => {
   counter.value ++;
 }
+
+const decrement = () =>{
+  counter.value --;
+}
+
+const reset = () => {
+  counter.value = 0;
+}
 </script>
 
 <template>
   <h1>Hola {{ name.toUpperCase() }}</h1>
-  <h2>{{counter}}</h2>
+  <h2 :class="counter > 0 ? 'positive' :'negative'" >{{counter}}</h2>
   <button @click="increment()">Aumentar</button>
+  <button @click="decrement()">Disminuir</button>
+  <button @click="reset()">Resetear</button>
 </template>
 
 <style>
 h1 {
   color: red;
+}
+.positive{
+  color:green;
+}
+.negative{
+  color:red;
 }
 </style>
