be196f856c60d87f157e89dfbb46c0012c9c8aff
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-03 10:13:43 -0600

    Initial commit: 🎉 project creation with Vue

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+const name = "Vue dinámico";
+const styleColor = "color: blue";
+const arrayColores = ["blue", "red", "peru"];
+const activo = true;
+</script>
+
+<template>
+  <h1>Hola {{ name.toUpperCase() }}</h1>
+  <h2>{{ arrayColores }}</h2>
+  <h2 :style="`color: ${arrayColores[2]}`">Soy Perú</h2>
+  <h2 v-if="activo === true">
+    <span>Icono</span>
+    Estoy activo
+  </h2>
+
+  <p v-else-if="activo === false">Estoy inactivo</p>
+  <p v-else>Estoy indeciso</p>
+
+  <h2 v-show="activo">Estoy activo v-show</h2>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
