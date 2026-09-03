ce4c76a5db1c2e54ca9b2b391a89bef97ad00fa5
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-03 12:52:46 -0600

    feat (v-on click) : ✨ `v-on` was used, along with its shorthand form `@`; two buttons were added as part of the click example.

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,22 @@
-<template>
-  <div>
-    <h2>Lista de usuarios:</h2>
-    <ul>
-      <template
-          v-for="(user, index) in users"
-          :key="user.id"
-      >
-        <li>
-          <h3>{{ user.name }}</h3>
-          <ul>
-            <template
-                v-for="(post, index) in user.posts"
-                :key="post.id"
-            >
-              <li>{{ post.title }}</li>
-            </template>
-          </ul>
-        </li>
-      </template>
-    </ul>
-  </div>
-</template>
+<!-- v-on se puede acortar con @ -->
 
 <script setup>
-const users = [
-  {
-    id: 1,
-    name: 'Juan',
-    posts: [
-      { id: 1, title: 'Mi primer post' },
-      { id: 2, title: 'Mi segundo post' },
-    ]
-  },
-  {
-    id: 2,
-    name: 'Maria',
-    posts: [
-      { id: 3, title: 'Mi tercer post' },
-      { id: 4, title: 'Mi cuarto post' },
-    ]
-  }
-]
+const name = "Vue dinámico";
+
+//metodo -methos
+const handleClick = () =>{
+  console.log('me diste click')
+}
 </script>
+
+<template>
+  <h1>Hola {{ name.toUpperCase() }}</h1>
+  <button v-on:click="handleClick()">Activame</button>
+  <button @click="handleClick()">Activame 2</button>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
