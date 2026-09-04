b1886d3ab9136eb10c9d6cea5ce8bb407a8c5cf5
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-04 11:53:28 -0600

    feat (Props): ✨ We use props and create a new file.

--- vue2/src/App.vue
+++ vue2/src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
 import ButtonCounter from "@/components/ButtonCounter.vue";
+import BlogPost from "@/components/BlogPost.vue";
 </script>
 
 <template>
   <div class="container">
-  <h1>APP</h1>
-  <buttonCounter/>
-  <buttonCounter/>
+    <h1>APP</h1>
+    <buttonCounter/>
+    <buttonCounter/>
+
+
+    <BlogPost tittle="Post 1"/>
+    <BlogPost tittle="Post 2"/>
+    <BlogPost tittle="Post 3"/>
+    <BlogPost tittle="Post 4"/>
   </div>
 </template>
 
