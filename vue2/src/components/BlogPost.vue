fac923d277ea687f9088e6f108207ce34dd53d05
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-04 15:56:21 -0600

    feat (referencias reactivas): ✨ Functions passed from child component to parent are not props; they are events.

--- vue2/src/components/BlogPost.vue
+++ vue2/src/components/BlogPost.vue
@@ -1,30 +1,28 @@
 <script setup>
-//defineProps(["title","id","body"])
-
 defineProps({
   title: String,
   id: Number,
   body: {
     type: String,
     default: 'Sin descripcion',
-  },
-  cambiarFavorito: Function
-})
+  }
+});
 
-const emit = defineEmits(['cambiarFavoritoNombre']);
-
-// Si deseas usar la función emit en la lógica de script, se hace así:
-// emit('cambiarFavoritoNombre', props.title);
-
-
+const emit = defineEmits(['cambiarFavorito']);
 </script>
 
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">{{id}}-{{title}}</h5>
-      <p>{{body}}</p>
-      <button @click="cambiarFavorito(title)" class="btn btn-outline-primary">Favorito</button>
+      <h5 class="card-title">{{ id }}-{{ title }}</h5>
+      <p>{{ body }}</p>
+
+      <button
+          @click="emit('cambiarFavorito', title)"
+          class="btn btn-outline-primary"
+      >
+        Favorito
+      </button>
     </div>
   </div>
 </template>
