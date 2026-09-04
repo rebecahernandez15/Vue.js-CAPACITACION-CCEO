ea15f000b89dd245720b1bcbc9a6c571002676e0
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-04 13:21:33 -0600

    feat (props-functions): ✨ We use `const` to modify `emit`, but it is not the most recommended approach, as the entire function is exposed.

--- vue2/src/components/BlogPost.vue
+++ vue2/src/components/BlogPost.vue
@@ -1,28 +1,29 @@
 <script setup>
-//defineProps(['tittle','id','body']);
-
 defineProps({
-  tittle:String,
-  id:Number,
-  body:{
-    type:String,
+  tittle: String,
+  id: Number,
+  body: {
+    type: String,
     default: "Sin descripcion"
-  },
+  }
 });
 
-const emit = defineEmits(['cambiarFavoritoNombre']);
-
-//emit('cambiarFavoritoNombre',tittle)
-
+const emit = defineEmits(['cambiarFavorito']);
 </script>
 
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">{{id}} - {{tittle}}</h5>
-      <p> {{body}}</p>
-      <button @click="emit('cambiarFavoritoNombre',tittle)"
-              class="btn btn-outline-primary">Favorito</button>
+      <h5 class="card-title">{{ id }} - {{ tittle }}</h5>
+      <p>{{ body }}</p>
+
+
+      <button
+          @click="emit('cambiarFavorito', tittle)"
+          class="btn btn-outline-primary"
+      >
+        Favorito
+      </button>
     </div>
   </div>
 </template>
