e527bc5fe6664c63e79a3cd8cf674fc41926433f
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-07 13:47:14 -0600

    feat (reutilizable): ✨ Instead of being one-time use, it was made reusable.

--- router/src/views/PokemonsView.vue
+++ router/src/views/PokemonsView.vue
@@ -2,7 +2,7 @@
 import { RouterLink } from "vue-router";
 import { useGetData } from "@/composables/getData";
 
-const { data, getData, loading } = useGetData();
+const { data, getData, loading, error } = useGetData();
 
 getData("https://pokeapi.co/api/v2/pokemon");
 </script>
@@ -10,13 +10,28 @@ getData("https://pokeapi.co/api/v2/pokemon");
 <template>
   <h1>Pokemons</h1>
   <p v-if="loading">Cargando información...</p>
+  <div class="alert alert-danger mt-2" v-if="error">{{ error }}</div>
   <div v-if="data">
     <ul>
-      <li v-for="poke in data.results">
+      <li v-for="poke in data.results" :key="poke.name">
         <router-link :to="`/pokemons/${poke.name}`">{{
             poke.name
           }}</router-link>
       </li>
     </ul>
+    <button
+        :disabled="!data.previous"
+        class="btn btn-success me-2"
+        @click="getData(data.previous)"
+    >
+      Previous
+    </button>
+    <button
+        :disabled="!data.next"
+        class="btn btn-primary"
+        @click="getData(data.next)"
+    >
+      Next
+    </button>
   </div>
 </template>
