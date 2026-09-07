bdf2576653222f47fcccc0106cdb4118e73f6673
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-07 14:02:18 -0600

    feat (detalles): ✨ Visual adjustments were made.

--- router/src/views/PokemonsView.vue
+++ router/src/views/PokemonsView.vue
@@ -12,26 +12,27 @@ getData("https://pokeapi.co/api/v2/pokemon");
   <p v-if="loading">Cargando información...</p>
   <div class="alert alert-danger mt-2" v-if="error">{{ error }}</div>
   <div v-if="data">
-    <ul>
-      <li v-for="poke in data.results" :key="poke.name">
+    <ul class="list-group">
+      <li v-for="poke in data.results" class="list-group-item">
         <router-link :to="`/pokemons/${poke.name}`">{{
             poke.name
           }}</router-link>
       </li>
     </ul>
-    <button
+    <div class="mt-2"><button
         :disabled="!data.previous"
         class="btn btn-success me-2"
         @click="getData(data.previous)"
     >
       Previous
     </button>
-    <button
-        :disabled="!data.next"
-        class="btn btn-primary"
-        @click="getData(data.next)"
-    >
-      Next
-    </button>
+      <button
+          :disabled="!data.next"
+          class="btn btn-primary"
+          @click="getData(data.next)"
+      >
+        Next
+      </button></div>
+
   </div>
 </template>
