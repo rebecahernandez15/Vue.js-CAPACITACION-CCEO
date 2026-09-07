13c864df7c9b65b29e613a9c0eff4f730fcb7774
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-07 16:30:59 -0600

    feat(componente favorito): ✨ We added a favorite component

--- router/src/App.vue
+++ router/src/App.vue
@@ -18,6 +18,7 @@ import { RouterLink, RouterView } from 'vue-router'
       <div>
         <router-link active-class="active" class="btn btn-outline-primary me-2" to="/">Home</router-link>
         <router-link active-class="active" class="btn btn-outline-primary" to="/pokemons">Pokemons</router-link>
+        <router-link active-class="active" class="btn btn-outline-primary" to="/favoritos">Favoritos</router-link>
       </div>
     </div>
   </nav>
