2e4aad6f912f14239269e4a058c0391a7402b568
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-07 11:47:01 -0600

    feat(optional-changing) : ✨we made some changes in home

--- router/src/views/PokeView.vue
+++ router/src/views/PokeView.vue
@@ -28,7 +28,7 @@ getData();
 </script>
 
 <template>
-  <img :src="poke.sprites.front_default" alt="">
+  <img :src="poke.sprites?.front_default" alt="">
   <h1>Poke name: {{ $route.params.name }}</h1>
   <button @click="back">Volver</button>
 </template>
