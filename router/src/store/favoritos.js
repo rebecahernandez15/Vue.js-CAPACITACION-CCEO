04984f32be84290c4b077b8fbc289baefd8225cd
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-07 17:15:30 -0600

    feat(remove-pokemon): ✨ We created the button to remove a Pokémon from favorites, and it works.

--- router/src/store/favoritos.js
+++ router/src/store/favoritos.js
@@ -1,16 +1,20 @@
-import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export const useFavoritosStore = defineStore('favoritos', () => {
-
-    const favoritos = ref([])
+export const useFavoritosStore = defineStore("favoritos", () => {
+    const favoritos = ref([]);
 
     const add = (poke) => {
-        favoritos.value.push(poke)
-    }
+        favoritos.value.push(poke);
+    };
+
+    const remove = (id) => {
+        favoritos.value = favoritos.value.filter((item) => item.id !== id);
+    };
 
     return {
         favoritos,
-        add
-    }
-})
+        add,
+        remove,
+    };
+});
--- router/src/views/FavoritosView.vue
+++ router/src/views/FavoritosView.vue
@@ -5,12 +5,17 @@ import { storeToRefs } from "pinia";
 const useFavoritos = useFavoritosStore();
 
 const { favoritos } = storeToRefs(useFavoritos);
+const { remove } = useFavoritos;
 </script>
 
 <template>
   <h1>Favoritos</h1>
-  <ul class="list-group">
+  <p v-if="favoritos.length === 0">Sin favoritos</p>
+  <ul class="list-group" v-else>
     <li class="list-group-item" v-for="poke in favoritos" :key="poke.id">
+      <div>
+        <button class="btn btn-sm btn-danger" @click="remove(poke.id)"> Eliminar </button>
+      </div>
       {{ poke.name }}
     </li>
   </ul>
