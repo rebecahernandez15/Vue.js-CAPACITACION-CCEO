665ccd98ccfd1da6df4586020171e289a123d588
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-07 10:45:24 -0600

    feat(Axios) : ✨All information about the selected Pokémon is obtained.

--- router/src/views/PokeView.vue
+++ router/src/views/PokeView.vue
@@ -1,3 +1,20 @@
+<script setup>
+import axios from "axios";
+
+const getData = async () => {
+  try {
+    const { data } = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/pikachu`
+    );
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getData();
+</script>
+
 <template>
   <h1>Poke name: {{ $route.params.name }}</h1>
 </template>
