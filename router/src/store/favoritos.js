c8eae9bffaffd3a9ac7e4c3bcdae01d8e084813d
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-07 16:41:41 -0600

    feat(agregar a favoritos): ✨ We're adding our Pokémon to the favorites section.

---
+++ router/src/store/favoritos.js
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useFavoritosStore = defineStore('favoritos', () => {
+
+    const favoritos = ref([])
+
+    const add = (poke) => {
+        favoritos.value.push(poke)
+    }
+
+    return {
+        favoritos,
+        add
+    }
+})
--- router/src/views/PokeView.vue
+++ router/src/views/PokeView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { useRoute, useRouter } from "vue-router";
 import { useGetData } from "@/composables/getData";
+import { useFavoritosStore } from '@/store/favoritos.js'
 
 const route = useRoute();
 const router = useRouter();
+const useFavoritos = useFavoritosStore();
+
+const { add } = useFavoritos;
 
 const { getData, data, loading, error } = useGetData();
 
@@ -13,13 +17,14 @@ const back = () => {
 
 getData(`https://pokeapi.co/api/v2/pokemon/${route.params.name}`);
 </script>
+
 <template>
   <p v-if="loading">Cargando información...</p>
   <div class="alert alert-danger mt-2" v-if="error">{{ error }}</div>
   <div v-if="data">
     <img :src="data.sprites?.front_default" alt="" />
     <h1>Poke name: {{ $route.params.name }}</h1>
-    <button class="back btn btn-outline-primary mb-2">Agregar favoritos</button>
+    <button class="back btn btn-outline-primary mb-2 " @click="add(data)">Agregar favoritos</button>
   </div>
   <button @click="back" class="btn btn-outline-primary">Volver</button>
 </template>
