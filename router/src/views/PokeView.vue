f9875e2378df816d2afe3ed7137153437fcb3e96
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-07 11:10:08 -0600

    feat(paint pokemons) : ✨We use `useRouter` in this exercise to see how it works also we added a button te get back.

--- router/src/views/PokeView.vue
+++ router/src/views/PokeView.vue
@@ -1,9 +1,12 @@
 <script setup>
 import axios from "axios";
+import {ref} from "vue";
 import {useRoute,useRouter} from 'vue-router'
 
 const route = useRoute();
 const router = useRouter();
+
+const poke = ref({})
 
 const back = () => {
   router.push('/pokemons')
@@ -15,6 +18,7 @@ const getData = async () => {
         `https://pokeapi.co/api/v2/pokemon/${route.params.name}`
     );
     console.log(data);
+    poke.value=data;
   } catch (error) {
     console.log(error);
   }
@@ -24,6 +28,7 @@ getData();
 </script>
 
 <template>
+  <img :src="poke.sprites.front_default" alt="">
   <h1>Poke name: {{ $route.params.name }}</h1>
   <button @click="back">Volver</button>
 </template>
